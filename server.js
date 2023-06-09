fffe6a8184222582d0afd38891355bb7a0edf5c4
MateuszMyszke <myszke93@gmail.com>
2023-06-09 14:18:10 +0200

    Add frontend, add interval

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const uuid = require('uuid');
 const router = express.Router();
 
@@ -10,6 +11,8 @@ const seatsRoutes = require('./routes/seats.routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
+
 
 app.use('/api/', testimonialsRoutes);
 app.use('/api/', concertsRoutes);
